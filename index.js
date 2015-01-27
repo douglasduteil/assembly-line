5ec7db01bf84d22b38e81600fe43f37b77e41ba2
Douglas Duteil <douglasduteil@gmail.com>
2015-01-28 00:08:03 +0100

    DRAFT

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-'use strict';
-
-var npm = require("npm");
-var path = require("path");
-var extend = require("util")._extend;
-
-require("./lib/install");
-exports.use = require("./lib/use");
+module.exports = require('./lib/assembly-line');
